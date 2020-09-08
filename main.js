d6c660baa5ada4a61fbc80d21d0f02cb856f48a9
x1nax <53822451+x1nax@users.noreply.github.com>
2020-09-08 09:31:20 +0700

    1

---
+++ main.js
@@ -0,0 +1,85 @@
+let difs = document.querySelectorAll('.difficult');
+let checks = document.querySelectorAll('#rhombus');
+let menu = document.querySelector(".menu");
+let play = document.querySelector(".btn");
+let table = document.querySelector(".table");
+let field = document.querySelector(".field");
+let cards = document.querySelectorAll(".table-card__wrap");
+let fronts = document.querySelectorAll(".card-loser");
+
+play.disabled = true;
+
+let clean = function () {
+  for(let check of checks){
+    if (check.classList.contains("difficult-check") === true){
+      check.classList.remove("difficult-check");
+    }
+  }
+  
+};
+
+let chooseDifs = function (dif, romb) {
+  dif.onclick = function () {
+    clean();
+    romb.classList.add('difficult-check');
+    switch (true) { 
+      case dif.innerHTML.indexOf("Простой") !== -1:
+          field.style.width = "726px";
+          break;
+      case dif.innerHTML.indexOf("Средний") !== -1:
+          field.style.width = "727px";
+          break;
+      case dif.innerHTML.indexOf("Сложный") !== -1:
+          field.style.width = "1220px";
+          break;        
+    };
+    play.innerHTML = "Начать игру";
+    play.disabled = false;
+  }
+}
+
+for (let i = 0; i <= difs.length-1; i++){
+  chooseDifs(difs[i],checks[i]);
+}
+
+function randomInteger(min, max) {
+  let rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+};
+
+play.onclick = function () {
+  menu.style.display = "none";
+  table.style.display = "flex";
+  console.log(field.style.width);
+  switch (field.style.width){
+    case "726px":
+      for(let i=0; i<3; i++){
+        cards[i].style.display = "flex";
+      }
+      fronts[randomInteger(0, 2)].src = "images/bug.png";
+      break;
+    case "727px":
+      for(let i=0; i<6; i++){
+        cards[i].style.display = "flex";
+      }
+      fronts[randomInteger(0, 5)].src = "images/bug.png";
+      break;
+    case "1220px":
+      for(let i=0; i<10; i++){
+        cards[i].style.display = "flex"; 
+      }
+      fronts[randomInteger(0, 9)].src = "images/bug.png";
+      break;
+  };
+}
+
+for(let card of cards){
+    card.onclick = function () {
+    card.classList.add('rotate');
+    for(let card of cards){
+      card.onclick = function () {
+        window.location.href=window.location.href  
+      }
+    }
+  } 
+}
